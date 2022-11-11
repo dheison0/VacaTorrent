bbcd896a1a8b34e82992e1499dc1cb6fa953639b
Dheisom <dheisomgomes0@gmail.com>
2022-11-10 21:48:18 -0300

    Rewrite Download screen using flatlist and same other things

--- src/screens/Download/index.js
+++ src/screens/Download/index.js
@@ -1,10 +1,34 @@
 import { Component } from 'react';
-import { Button, ScrollView, Text, Image, View, Linking, ToastAndroid } from 'react-native';
+import { Button, ScrollView, Text, Image, View, Linking, ToastAndroid, FlatList } from 'react-native';
 import { url } from '../../vars.js';
 import axios from 'axios';
 import styles from './styles.js';
 import Loading from '../../components/Loading.js';
 import Error from '../../components/Error.js';
+
+const IMDB = ({ rating }) => (
+  <View style={styles.imdb}>
+    <Image
+      style={{ width: 26, height: 26 }}
+      source={require('../../../assets/star.png')}
+    />
+    <Text style={styles.imdbRating}>{rating}</Text>
+  </View>
+);
+
+const LinkButton = ({ item }) => (
+  <View style={styles.link}>
+    <Button
+      title={item.title}
+      onPress={() => {
+        Linking.openURL(item.url).catch(() => ToastAndroid.show(
+          "Não foi possivel abrir o link!\nTalvez você não possua nenhum app compativel com Torrent",
+          ToastAndroid.LONG
+        ))
+      }}
+    />
+  </View>
+);
 
 const Download = ({ data }) => (
   <ScrollView style={{ flex: 1 }}>
@@ -12,66 +36,64 @@ const Download = ({ data }) => (
       <View style={styles.base}>
         <Image style={styles.thumbnail} source={{ uri: data.thumbnail }} />
         <Text style={styles.title}>{data.title}</Text>
-        {(() => {
-          if (data.imdb > 0) {
-            return (
-              <View style={styles.imdb}>
-                <Image
-                  style={{ width: 26, height: 26 }}
-                  source={require('../../../assets/star.png')}
-                />
-                <Text style={styles.imdbRating}>{data.imdb}</Text>
-              </View>
-            );
-          }
-        })()}
+        {data.imdb > 0 ? (<IMDB rating={data.imdb} />) : null}
       </View>
       <Text style={styles.sinopse}>{data.sinopse}</Text>
-      {data.links.map((link, index) => (
-        <View style={styles.link} key={index}>
-          <Button
-            title={link.title}
-            onPress={() => {
-              Linking.openURL(link.url).catch(() => ToastAndroid.show(
-                "Não foi possivel abrir o link!\nTalvez você não possua nenhum app compativel com Torrent",
-                ToastAndroid.LONG
-              ))
-            }}
-          />
-        </View>
-      ))}
+      <FlatList
+        data={data.links}
+        renderItem={LinkButton}
+      />
     </View>
   </ScrollView>
 );
 
 class DownloadScreen extends Component {
+  state = {
+    error: null,
+    isLoading: true,
+    result: null,
+  }
   constructor(props) {
     super(props);
-    this.state = { content: (<Loading msg="Buscando informações..." />) };
     this.getMovieInformations();
   }
   getMovieInformations() {
-    this.setState({ content: (<Loading msg="Buscando informações..." />) });
+    this.setState({ isLoading: true });
     axios.get(
       `${url}/download/${this.props.route.params.url}`,
       { timeout: 10000 }
     ).then(response => {
-      this.setState({ content: (<Download data={response.data} />) });
+      this.setState({
+        isLoading: false,
+        result: response.data,
+      });
     }).catch(error => {
       this.setState({
-        content: (
-          <Error
-            msg={`Erro obtendo informações de download:\n${error.toString()}`}
-            onRetry={() => this.getMovieInformations()}
-          />
-        )
+        isLoading: false,
+        error: `Erro obtendo informações de download:\n${error.toString()}`
       });
     });
+  }
+  whenLoading() {
+    return (<Loading msg="Buscando informações..." />);
+  }
+  onSuccess() {
+    return (<Download data={this.state.result} />);
+  }
+  onError() {
+    return (
+      <Error
+        msg={this.state.error}
+        onRetry={() => this.getMovieInformations()}
+      />
+    )
   }
   render() {
     return (
       <View style={{ flex: 1 }}>
-        {this.state.content}
+        {this.state.isLoading ? this.whenLoading() : (
+          this.state.error ? this.onError() : this.onSuccess()
+        )}
       </View>
     );
   }
